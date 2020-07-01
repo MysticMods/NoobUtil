3cb3c6b894e3d5d644d233c71a3c0e5d5440c7fb
Jon McManus <due@wxwhatever.com>
2020-07-01 21:46:05 +1000

    Tweak things.

--- src/main/java/noobanidus/libs/noobutil/registrate/FeatureBuilder.java
+++ src/main/java/noobanidus/libs/noobutil/registrate/FeatureBuilder.java
@@ -11,22 +11,20 @@ import java.util.function.Function;
 public class FeatureBuilder<FC extends IFeatureConfig, T extends Feature<FC>, P> extends AbstractBuilder<Feature<?>, T, P, FeatureBuilder<FC, T, P>> {
   private IFactory<FC, T> factory;
   private Function<Dynamic<?>, FC> configFactory;
-  private boolean doBlockNotify;
 
-  public FeatureBuilder(CustomRegistrate owner, P parent, String name, BuilderCallback callback, Function<Dynamic<?>, FC> configfactory, boolean doBlockNotify, IFactory<FC, T> factory) {
+  public FeatureBuilder(CustomRegistrate owner, P parent, String name, BuilderCallback callback, Function<Dynamic<?>, FC> configfactory, IFactory<FC, T> factory) {
     super(owner, parent, name, callback, Feature.class);
     this.factory = factory;
     this.configFactory = configfactory;
-    this.doBlockNotify = doBlockNotify;
   }
 
   @Override
   protected T createEntry() {
-    return factory.create(configFactory, doBlockNotify);
+    return factory.create(configFactory);
   }
 
   @FunctionalInterface
   public interface IFactory<FC extends IFeatureConfig, T extends Feature<FC>> {
-    T create(Function<Dynamic<?>, FC> factory, boolean doBlockNotify);
+    T create(Function<Dynamic<?>, FC> factory);
   }
 }
