fa4a3c4bb50535ee5bb89c65265f948d6c5e72e7
Jon McManus <due@wxwhatever.com>
2020-08-11 15:43:33 +1000

    Missed these.

---
+++ src/main/java/noobanidus/libs/noobutil/advancement/IGenericPredicate.java
@@ -0,0 +1,12 @@
+package noobanidus.libs.noobutil.advancement;
+
+import com.google.gson.JsonElement;
+import net.minecraft.entity.player.ServerPlayerEntity;
+
+import javax.annotation.Nullable;
+
+public interface IGenericPredicate<T> {
+  boolean test(ServerPlayerEntity player, T condition);
+
+  IGenericPredicate<T> deserialize(@Nullable JsonElement element);
+}
