9cfc0a11f693de895a9e4b4193dc80f6877aff01
Jon McManus <due@wxwhatever.com>
2020-11-22 16:45:30 +1000

    Phase out the player modifier registry.

--- src/main/java/noobanidus/libs/noobutil/setup/ShadedCommonSetup.java
+++ src/main/java/noobanidus/libs/noobutil/setup/ShadedCommonSetup.java
@@ -1,11 +1,8 @@
 package noobanidus.libs.noobutil.setup;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import noobanidus.libs.noobutil.modifiers.PlayerModifierRegistry;
 
 public class ShadedCommonSetup {
-  public static void init (FMLCommonSetupEvent event) {
-    MinecraftForge.EVENT_BUS.addListener(PlayerModifierRegistry.getInstance()::onEntityConstructed);
+  public static void init(FMLCommonSetupEvent event) {
   }
 }
