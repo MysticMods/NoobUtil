48375496fafa7b7d1dc29118832d6017fbac48c3
Jon McManus <due@wxwhatever.com>
2020-12-01 20:00:56 +1000

    Tweak UtilRegistry.
    This is so dumb when you think about it.

--- src/main/java/noobanidus/libs/noobutil/init/UtilRegistry.java
+++ src/main/java/noobanidus/libs/noobutil/init/UtilRegistry.java
@@ -1,5 +1,6 @@
 package noobanidus.libs.noobutil.init;
 
+import com.tterrag.registrate.Registrate;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import net.minecraft.world.gen.blockstateprovider.BlockStateProviderType;
 import noobanidus.libs.noobutil.types.SupplierBlockStateProvider;
@@ -7,10 +8,7 @@ import noobanidus.libs.noobutil.types.SupplierBlockStateProvider;
 public class UtilRegistry {
   public static RegistryEntry<BlockStateProviderType<SupplierBlockStateProvider>> SUPPLIER_STATE_PROVIDER;
 
-  public static void load (RegistryEntry<BlockStateProviderType<SupplierBlockStateProvider>> supplierStateProvider) {
-    // TODO: Gosh, this is problematic
-    if (SUPPLIER_STATE_PROVIDER == null) {
-      SUPPLIER_STATE_PROVIDER = supplierStateProvider;
-    }
+  public static <T extends Registrate> void load (T registry) {
+    SUPPLIER_STATE_PROVIDER = registry.simple("supplier_state_provider", BlockStateProviderType.class, () -> new BlockStateProviderType<>(SupplierBlockStateProvider.CODEC));
   }
 }
