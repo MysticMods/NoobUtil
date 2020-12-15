dee76e00ddd2a80f5d59da708b9bdbb2330520f6
Jon McManus <due@wxwhatever.com>
2020-12-15 20:20:06 +1000

    Double config.

--- src/main/java/noobanidus/libs/noobutil/config/BiomeConfig.java
+++ src/main/java/noobanidus/libs/noobutil/config/BiomeConfig.java
@@ -6,21 +6,25 @@ import java.util.Collection;
 
 public class BiomeConfig {
   private String name;
-  private int weight;
+  private double weight;
 
-  private ForgeConfigSpec.IntValue configWeight;
+  private ForgeConfigSpec.DoubleValue configWeight;
 
-  private BiomeConfig(String name, int weight, Collection<BiomeConfig> list) {
+  private BiomeConfig (String name, double weight, Collection<BiomeConfig> list) {
     this.name = name;
     this.weight = weight;
     list.add(this);
+  }
+
+  private BiomeConfig(String name, int weight, Collection<BiomeConfig> list) {
+    this(name, (double) weight, list);
   }
 
   public String name() {
     return name;
   }
 
-  public int weight() {
+  public double weight() {
     return configWeight.get();
   }
 
@@ -30,7 +34,7 @@ public class BiomeConfig {
 
   public void apply(ForgeConfigSpec.Builder builder) {
     builder.comment(name + " biome generation").push(name + "_biome");
-    configWeight = builder.comment("spawn weight of the biome (0 to disable generation)").defineInRange("weight", weight, 0, Integer.MAX_VALUE);
+    configWeight = builder.comment("spawn weight of the biome (0 to disable generation)").defineInRange("weight", weight, 0.0, Double.MAX_VALUE);
     builder.pop();
   }
 
@@ -42,6 +46,10 @@ public class BiomeConfig {
     }
 
     public BiomeConfig build(String name, int weight) {
+      return new BiomeConfig(name, weight, this.list);
+    }
+
+    public BiomeConfig build (String name, double weight) {
       return new BiomeConfig(name, weight, this.list);
     }
   }
