e3d86f44eb7e48283bdaf9739bad99759f60290b
Jon McManus <due@wxwhatever.com>
2020-12-20 10:35:20 +1000

    WeightedLakesFeature.

---
+++ src/main/java/noobanidus/libs/noobutil/world/gen/config/WeightedBlockStateFeatureConfig.java
@@ -0,0 +1,31 @@
+package noobanidus.libs.noobutil.world.gen.config;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.blockstateprovider.WeightedBlockStateProvider;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+public class WeightedBlockStateFeatureConfig implements IFeatureConfig {
+  public static final Codec<WeightedBlockStateFeatureConfig> CODEC = WeightedBlockStateProvider.CODEC.fieldOf("states").xmap(WeightedBlockStateFeatureConfig::new, (o) -> o.states).codec();
+  private final WeightedBlockStateProvider states;
+  private final Set<BlockState> containedStates = new HashSet<>();
+
+  public WeightedBlockStateFeatureConfig(WeightedBlockStateProvider states) {
+    this.states = states;
+    this.states.weightedStates.field_220658_a.forEach(o -> containedStates.add(o.func_220647_b()));
+  }
+
+  public BlockState getBlockState (Random random) {
+    return this.states.getBlockState(random, BlockPos.ZERO);
+  }
+
+  public boolean containsState (BlockState state) {
+    return containedStates.contains(state);
+  }
+}
