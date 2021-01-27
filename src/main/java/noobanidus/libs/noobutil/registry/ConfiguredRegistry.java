d8a89493dd0bd3d209b2d904278371af937a1e02
Jon McManus <due@wxwhatever.com>
2021-01-27 12:56:34 +1000

    Experimental registration system.

---
+++ src/main/java/noobanidus/libs/noobutil/registry/ConfiguredRegistry.java
@@ -0,0 +1,38 @@
+package noobanidus.libs.noobutil.registry;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.Registry;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConfiguredRegistry<T> {
+  private final Map<String, T> entries = new ConcurrentHashMap<>();
+  private final Registry<T> registry;
+  private boolean registered = false;
+  private final String modid;
+
+  public ConfiguredRegistry(String modid, Registry<T> registry) {
+    this.registry = registry;
+    this.modid = modid;
+  }
+
+  public T register(String name, T value) {
+    T current = entries.get(name);
+    if (current != null) {
+      throw new IllegalArgumentException("Key '" + name + "' already exists for this registry.");
+    }
+    entries.put(name, value);
+    return value;
+  }
+
+  public void registration() {
+    if (registered) {
+      throw new IllegalStateException("Attempted to register configured registry " + this + " when it has already been marked as registered.");
+    }
+    for (Map.Entry<String, T> entry : entries.entrySet()) {
+      Registry.register(this.registry, new ResourceLocation(modid, entry.getKey()), entry.getValue());
+    }
+    registered = true;
+  }
+}
