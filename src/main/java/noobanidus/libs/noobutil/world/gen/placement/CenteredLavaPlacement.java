3a3dbf3b4671eb7a5f3a519930227575163fe2ac
Jon McManus <due@wxwhatever.com>
2021-02-10 10:28:48 +1000

    Centered features.

---
+++ src/main/java/noobanidus/libs/noobutil/world/gen/placement/CenteredLavaPlacement.java
@@ -0,0 +1,29 @@
+package noobanidus.libs.noobutil.world.gen.placement;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.WorldDecoratingHelper;
+import net.minecraft.world.gen.placement.ChanceConfig;
+import net.minecraft.world.gen.placement.Placement;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class CenteredLavaPlacement extends Placement<ChanceConfig> {
+   public CenteredLavaPlacement(Codec<ChanceConfig> codec) {
+      super(codec);
+   }
+
+   public Stream<BlockPos> getPositions(WorldDecoratingHelper helper, Random rand, ChanceConfig config, BlockPos pos) {
+      if (rand.nextInt(config.chance / 10) == 0) {
+         int i = 8 + pos.getX();
+         int j = 8 + pos.getZ();
+         int k = rand.nextInt(rand.nextInt(helper.func_242891_a() - 8) + 8);
+         if (k < helper.func_242895_b() || rand.nextInt(config.chance / 8) == 0) {
+            return Stream.of(new BlockPos(i, k, j));
+         }
+      }
+
+      return Stream.empty();
+   }
+}
---
+++ src/main/java/noobanidus/libs/noobutil/world/gen/placement/CenteredWaterPlacement.java
@@ -0,0 +1,27 @@
+package noobanidus.libs.noobutil.world.gen.placement;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.gen.feature.WorldDecoratingHelper;
+import net.minecraft.world.gen.placement.ChanceConfig;
+import net.minecraft.world.gen.placement.Placement;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class CenteredWaterPlacement extends Placement<ChanceConfig> {
+  public CenteredWaterPlacement(Codec<ChanceConfig> codec) {
+    super(codec);
+  }
+
+  public Stream<BlockPos> getPositions(WorldDecoratingHelper helper, Random rand, ChanceConfig config, BlockPos pos) {
+    if (rand.nextInt(config.chance) == 0) {
+      int i = 8 + pos.getX();
+      int j = 8 + pos.getZ();
+      int k = rand.nextInt(helper.func_242891_a());
+      return Stream.of(new BlockPos(i, k, j));
+    } else {
+      return Stream.empty();
+    }
+  }
+}
