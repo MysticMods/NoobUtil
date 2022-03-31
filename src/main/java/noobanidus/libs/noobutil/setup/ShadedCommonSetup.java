b89d98b7d66e7f72b2338ea040cccc947a9c06ac
Jon McManus <due@wxwhatever.com>
2022-03-31 11:37:49 +1000

    Remove deleted imprt.

--- src/main/java/noobanidus/libs/noobutil/setup/ShadedCommonSetup.java
+++ src/main/java/noobanidus/libs/noobutil/setup/ShadedCommonSetup.java
@@ -1,7 +1,6 @@
 package noobanidus.libs.noobutil.setup;
 
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import noobanidus.libs.noobutil.modifier.PlayerModifierRegistry;
 
 public class ShadedCommonSetup {
   public static void init(FMLCommonSetupEvent event) {
