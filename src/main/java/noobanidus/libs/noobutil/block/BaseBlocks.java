763064935ea08ba03e9335218621f1b6f6614281
Jon McManus <due@wxwhatever.com>
2022-05-06 10:07:08 +1000

    Move wild crop into this.

--- src/main/java/noobanidus/libs/noobutil/block/BaseBlocks.java
+++ src/main/java/noobanidus/libs/noobutil/block/BaseBlocks.java
@@ -1,8 +1,12 @@
 package noobanidus.libs.noobutil.block;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -48,6 +52,42 @@ public class BaseBlocks {
     @Override
     protected ItemLike getBaseSeedId() {
       return seedProvider.get().get();
+    }
+  }
+
+  public class WildCropBlock extends BushBlock {
+    private static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    private final TagKey<Block> supporter;
+
+    public WildCropBlock(BlockBehaviour.Properties builder, TagKey<Block> tag) {
+      super(builder);
+      this.supporter = tag;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+      return SHAPE;
+    }
+
+    @Override
+    protected boolean mayPlaceOn(BlockState state, BlockGetter worldIn, BlockPos pos) {
+      return state.is(this.supporter);
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState state) {
+      return false;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void randomTick(BlockState state, ServerLevel worldIn, BlockPos pos, Random random) {
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader worldIn, BlockPos pos) {
+      return this.mayPlaceOn(worldIn.getBlockState(pos.below()), worldIn, pos.below());
     }
   }
 
